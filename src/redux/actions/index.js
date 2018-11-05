66a9c9e7dc0d24b1d8e9aaa35d135f0b06629880
Alex Brown <alexanderbrown.info@gmail.com>
2018-11-05 19:11:52 +0000

    install axios and create async actions

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,13 +1,14 @@
+import { get } from 'axios'
 export const FETCH_DATA_LOADING = 'FETCH_DATA_LOADING'
 export const FETCH_DATA_ERROR = 'FETCH_DATA_ERROR'
 export const FETCH_DATA_SUCCESS = 'FETCH_DATA_SUCCESS'
 
-export const fetchServicesLoading = (isLoading) => ({
+export const fetchServicesLoading = () => ({
   type: FETCH_DATA_LOADING,
   name: 'services'
 })
 
-export const fetchServicesError = (hasError) => ({
+export const fetchServicesError = () => ({
   type: FETCH_DATA_ERROR,
   name: 'services'
 })
@@ -18,12 +19,12 @@ export const fetchServicesSuccess = (payload) => ({
   payload
 })
 
-export const fetchBikesLoading = (isLoading) => ({
+export const fetchBikesLoading = () => ({
   type: FETCH_DATA_LOADING,
   name: 'bikes'
 })
 
-export const fetchBikesError = (hasError) => ({
+export const fetchBikesError = () => ({
   type: FETCH_DATA_ERROR,
   name: 'bikes'
 })
@@ -33,3 +34,27 @@ export const fetchBikesSuccess = (payload) => ({
   name: 'bikes',
   payload
 })
+
+export const fetchServices = () => dispatch => {
+  dispatch(fetchServicesLoading())
+  let servicesURL = 'https://api.tfl.gov.uk/Line/Mode/tube,overground,dlr/Status?detail=true'
+  get(servicesURL)
+    .then(response => {
+      dispatch(fetchServicesSuccess(response))
+    })
+    .catch(error => {
+      dispatch(fetchServicesError())
+    })
+}
+
+export const fetchBikes = (query) => dispatch => {
+  dispatch(fetchBikesLoading())
+  let servicesURL = `https://api.tfl.gov.uk/BikePoint/Search?query=${query}`
+  get(servicesURL)
+    .then(response => {
+      dispatch(fetchBikesSuccess(response))
+    })
+    .catch(error => {
+      dispatch(fetchBikesError())
+    })
+}
